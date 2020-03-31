2b0f8abcbe99572da6bd206d92bbf1cf4e467bac
samip23 <62965898+samip23@users.noreply.github.com>
2020-03-31 19:53:15 -0400

    Add files via upload

--- resource/Database.php
+++ resource/Database.php
@@ -1,12 +1,22 @@
 <?php
 //initialize variables to hold connection parameters
+$db = parse_url(getenv("DATABASE_URL"));
 $username = 'root';
 $dsn = 'mysql:host=127.0.0.1; dbname=register';
 $password = 'MAST3RIP77';
 
 try{
     //create an instance of the PDO class with the required parameters
-    $db = new PDO($dsn, $username, $password);
+    //$db = new PDO($dsn, $username, $password);
+
+    $pdo = new PDO("pgsql:" . sprintf(
+            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+            $db["DATABASE_URL"],
+            $db[""],
+            $db["Sam"],
+            $db[""],
+            ltrim($db["path"], "/")
+        ));
 
     //set pdo error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
