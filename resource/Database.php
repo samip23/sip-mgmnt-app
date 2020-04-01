cc77cc04c6c4af08ce3cffad856928c3546a057e
samip23 <62965898+samip23@users.noreply.github.com>
2020-03-31 22:10:09 -0400

    Add files via upload

--- resource/Database.php
+++ resource/Database.php
@@ -1,22 +1,19 @@
 <?php
 //initialize variables to hold connection parameters
-$db = parse_url(getenv("DATABASE_URL"));
-$username = 'ulgqhgizgnazhw';
-$dsn = 'psql:host=ec2-34-202-7-83.compute-1.amazonaws.com; dbname=ddk4hi9q2quf5j';
-$password = '3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb';
+
+$host="ec2-34-202-7-83.compute-1.amazonaws.com";
+$user = "ulgqhgizgnazhw";
+$password="3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb";
+$dbname="ddk4hi9q2quf5j";
+$port="5432";
 
 try{
     //create an instance of the PDO class with the required parameters
     //$db = new PDO($dsn, $username, $password);
     
-    $pdo = new PDO("pgsql:" . sprintf(
-            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-            $db["ec2-34-202-7-83.compute-1.amazonaws.com"],
-            $db["5432"],
-            $db["ulgqhgizgnazhw"],
-            $db["3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb"],
-            ltrim($db["path"], "/")
-        ));
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+    $pdo = new PDO($dsn, $user, $password);
 
     //set pdo error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
