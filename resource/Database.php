2623712d24c0e2f5f71426434185c74f5630deb1
samip23 <62965898+samip23@users.noreply.github.com>
2020-03-31 21:38:24 -0400

    Add files via upload

--- resource/Database.php
+++ resource/Database.php
@@ -1,20 +1,20 @@
 <?php
 //initialize variables to hold connection parameters
 $db = parse_url(getenv("DATABASE_URL"));
-$username = 'root';
-$dsn = 'mysql:host=127.0.0.1; dbname=register';
-$password = 'MAST3RIP77';
+$username = 'ulgqhgizgnazhw';
+$dsn = 'psql:host=ec2-34-202-7-83.compute-1.amazonaws.com; dbname=ddk4hi9q2quf5j';
+$password = '3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb';
 
 try{
     //create an instance of the PDO class with the required parameters
     //$db = new PDO($dsn, $username, $password);
-
+    
     $pdo = new PDO("pgsql:" . sprintf(
             "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-            $db["DATABASE_URL"],
-            $db[""],
-            $db["Sam"],
-            $db[""],
+            $db["ec2-34-202-7-83.compute-1.amazonaws.com"],
+            $db["5432"],
+            $db["ulgqhgizgnazhw"],
+            $db["3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb"],
             ltrim($db["path"], "/")
         ));
 
