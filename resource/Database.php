202ee224f2d4dcabc97fd5a6e1d4f678f8ee66e0
Sam Ip <ip.sam23@gmail.com>
2020-03-31 21:50:48 -0400

    database setup

--- resource/Database.php
+++ resource/Database.php
@@ -1,12 +1,22 @@
 <?php
 //initialize variables to hold connection parameters
-$username = 'root';
-$dsn = 'mysql:host=127.0.0.1; dbname=register';
-$password = 'MAST3RIP77';
+$db = parse_url(getenv("DATABASE_URL"));
+$username = 'ulgqhgizgnazhw';
+$dsn = 'psql:host=ec2-34-202-7-83.compute-1.amazonaws.com; dbname=ddk4hi9q2quf5j';
+$password = '3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb';
 
 try{
     //create an instance of the PDO class with the required parameters
-    $db = new PDO($dsn, $username, $password);
+    //$db = new PDO($dsn, $username, $password);
+    
+    $pdo = new PDO("pgsql:" . sprintf(
+            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+            $db["ec2-34-202-7-83.compute-1.amazonaws.com"],
+            $db["5432"],
+            $db["ulgqhgizgnazhw"],
+            $db["3d0f9251d4892fbb4527cd5af5912f7c3b92d38cab3c095eb3240e376c2736bb"],
+            ltrim($db["path"], "/")
+        ));
 
     //set pdo error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
